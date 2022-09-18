1e3690dfc0a3af33e03bda84acd6f95b2a414ab5
artem32777 <109870624+artem32777@users.noreply.github.com>
2022-09-19 02:23:19 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4552,7 +4552,7 @@
             watchOverflow: true,
             freeMode: false,
             loop: true,
-            loopAdditionalSlides: 20,
+            loopAdditionalSlides: 50,
             autoplay: {
                 delay: 0,
                 disableOnInteraction: false,
@@ -4582,7 +4582,7 @@
             loop: true,
             watchOverflow: true,
             freeMode: true,
-            loopAdditionalSlides: 20,
+            loopAdditionalSlides: 50,
             slidesOffsetBefore: 1500,
             autoplay: {
                 reverseDirection: true,
@@ -4615,7 +4615,7 @@
             loop: true,
             watchOverflow: true,
             freeMode: true,
-            loopAdditionalSlides: 20,
+            loopAdditionalSlides: 50,
             autoplay: {
                 reverseDirection: true,
                 delay: 1e3,
@@ -4772,6 +4772,89 @@
     };
     const da = new DynamicAdapt("max");
     da.init();
+    const SCROLLABLE_CLASSNAME = "fs--scrollable";
+    const PREVENT_SCROLL_DATASET = "fsScrollable";
+    const DELTA_DATASET = "fsDelta";
+    const findTarget = e => {
+        let target = e.target;
+        while (null !== target) {
+            if (target.classList && target.classList.contains(SCROLLABLE_CLASSNAME)) break;
+            target = target.parentNode;
+        }
+        return target;
+    };
+    const touchstartEventHandler = (e, fixScroll) => {
+        const target = findTarget(e);
+        if (target) {
+            target.scrollTop;
+            const totalScroll = target.scrollHeight;
+            const height = target.clientHeight;
+            target.dataset[DELTA_DATASET] = e.touches[0].clientY;
+            if (height === totalScroll) target.dataset[PREVENT_SCROLL_DATASET] = "true";
+        }
+    };
+    const touchmoveEventHandler = (e, fixScroll) => {
+        if (!fixScroll.getState()) {
+            const target = findTarget(e);
+            if (target) if ("true" === target.dataset[PREVENT_SCROLL_DATASET]) e.preventDefault(); else {
+                const scrollTop = target.scrollTop;
+                const totalScroll = target.scrollHeight;
+                const currentScroll = scrollTop + target.offsetHeight;
+                const delta = parseFloat(target.dataset[DELTA_DATASET]);
+                const currentDelta = e.touches[0].clientY;
+                if (scrollTop <= 0) {
+                    if (delta < currentDelta) e.preventDefault();
+                } else if (currentScroll >= totalScroll) if (delta > currentDelta) e.preventDefault();
+            }
+        }
+    };
+    const touchendEventHandler = (e, fixScroll) => {
+        const target = findTarget(e);
+        if (target) target.dataset[PREVENT_SCROLL_DATASET] = "false";
+    };
+    const bindEvents = fixScroll => {
+        document.addEventListener("touchstart", (e => touchstartEventHandler(e, fixScroll)));
+        document.addEventListener("touchmove", (e => touchmoveEventHandler(e, fixScroll)));
+        document.addEventListener("touchend", (e => touchendEventHandler(e, fixScroll)));
+    };
+    class FixScroll {
+        constructor() {
+            this.state = true;
+            bindEvents(this);
+        }
+        getState() {
+            return this.state;
+        }
+        hide() {
+            const currentWidth = this.getCurrentWidth();
+            document.body.style.overflow = "hidden";
+            document.body.style.paddingRight = currentWidth + "px";
+            this.state = false;
+        }
+        show() {
+            document.body.style.overflow = "";
+            document.body.style.paddingRight = "";
+            this.state = true;
+        }
+        toggle() {
+            if (this.getState()) this.hide(); else this.show();
+        }
+        getWidth() {
+            const overflowCurrentProperty = document.body.style.overflow;
+            let width = 0;
+            document.body.style.overflow = "scroll";
+            width = this.getCurrentWidth();
+            document.body.style.overflow = overflowCurrentProperty;
+            return width;
+        }
+        getCurrentWidth() {
+            const documentWidth = document.documentElement.clientWidth;
+            const windowWidth = window.innerWidth;
+            const currentWidth = windowWidth - documentWidth;
+            return currentWidth;
+        }
+    }
+    new FixScroll;
     window["FLS"] = true;
     isWebp();
     menuInit();
