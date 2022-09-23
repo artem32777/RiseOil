364b1e496b3affc96a0cea34230fde502e858e9f
artem32777 <109870624+artem32777@users.noreply.github.com>
2022-09-23 18:48:22 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4617,7 +4617,6 @@
             freeMode: true,
             loopAdditionalSlides: 50,
             autoplay: {
-                reverseDirection: true,
                 delay: 1e3,
                 disableOnInteraction: false,
                 stopOnLastSlide: false,
@@ -4650,7 +4649,6 @@
             freeMode: true,
             loopAdditionalSlides: 50,
             autoplay: {
-                reverseDirection: true,
                 delay: 1e3,
                 disableOnInteraction: false,
                 stopOnLastSlide: false,
@@ -5020,6 +5018,24 @@
         }), 500);
     };
     new FixScroll;
+    let inputPhone = document.getElementById("inputPhone");
+    inputPhone.oninput = () => phoneMask(inputPhone);
+    function phoneMask(inputEl) {
+        let patStringArr = "+7(___)___-__-__".split("");
+        let arrPush = [ 3, 4, 5, 7, 8, 9, 11, 12, 14, 15 ];
+        let val = inputEl.value;
+        let arr = val.replace(/\D+/g, "").split("").splice(1);
+        let n;
+        let ni;
+        arr.forEach(((s, i) => {
+            n = arrPush[i];
+            patStringArr[n] = s;
+            ni = i;
+        }));
+        arr.length < 10 ? inputEl.style.color = "grey" : inputEl.style.color = "green";
+        inputEl.value = patStringArr.join("");
+        n ? inputEl.setSelectionRange(n + 1, n + 1) : inputEl.setSelectionRange(17, 17);
+    }
     window["FLS"] = true;
     isWebp();
     menuInit();
