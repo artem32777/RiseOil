85bfc5f82864f13ea3daf2afae41314b03a25ef5
xxx63124 <109870624+artem32777@users.noreply.github.com>
2023-01-22 00:45:05 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -396,26 +396,6 @@
                 document.documentElement.classList.add(className);
             }));
         }
-        let isMobile = {
-            Android: function() {
-                return navigator.userAgent.match(/Android/i);
-            },
-            BlackBerry: function() {
-                return navigator.userAgent.match(/BlackBerry/i);
-            },
-            iOS: function() {
-                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-            },
-            Opera: function() {
-                return navigator.userAgent.match(/Opera Mini/i);
-            },
-            Windows: function() {
-                return navigator.userAgent.match(/IEMobile/i);
-            },
-            any: function() {
-                return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
-            }
-        };
         function addLoadedClass() {
             window.addEventListener("load", (function() {
                 setTimeout((function() {
@@ -429,17 +409,6 @@
         function setHash(hash) {
             hash = hash ? `#${hash}` : window.location.href.split("#")[0];
             history.pushState("", "", hash);
-        }
-        function fullVHfix() {
-            const fullScreens = document.querySelectorAll("[data-fullscreen]");
-            if (fullScreens.length && isMobile.any()) {
-                window.addEventListener("resize", fixHeight);
-                function fixHeight() {
-                    let vh = .01 * window.innerHeight;
-                    document.documentElement.style.setProperty("--vh", `${vh}px`);
-                }
-                fixHeight();
-            }
         }
         let _slideUp = (target, duration = 500, showmore = 0) => {
             if (!target.classList.contains("_slide")) {
@@ -731,11 +700,7 @@
                 }
             }));
         }
-        function menuClose() {
-            bodyUnlock();
-            document.documentElement.classList.remove("menu-open");
-        }
-        function FLS(message) {
+        function functions_FLS(message) {
             setTimeout((() => {
                 if (window.FLS) console.log(message);
             }), 0);
@@ -1024,208 +989,10 @@
                 if (!this.isOpen && this.lastFocusEl) this.lastFocusEl.focus(); else focusable[0].focus();
             }
             popupLogging(message) {
-                this.options.logging ? FLS(`[Попапос]: ${message}`) : null;
+                this.options.logging ? functions_FLS(`[Попапос]: ${message}`) : null;
             }
         }
         flsModules.popup = new Popup({});
-        let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
-            const targetBlockElement = document.querySelector(targetBlock);
-            if (targetBlockElement) {
-                let headerItem = "";
-                let headerItemHeight = 0;
-                if (noHeader) {
-                    headerItem = "header.header";
-                    headerItemHeight = document.querySelector(headerItem).offsetHeight;
-                }
-                let options = {
-                    speedAsDuration: true,
-                    speed,
-                    header: headerItem,
-                    offset: offsetTop,
-                    easing: "easeOutQuad"
-                };
-                document.documentElement.classList.contains("menu-open") ? menuClose() : null;
-                if ("undefined" !== typeof SmoothScroll) (new SmoothScroll).animateScroll(targetBlockElement, "", options); else {
-                    let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
-                    targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
-                    targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
-                    window.scrollTo({
-                        top: targetBlockElementPosition,
-                        behavior: "smooth"
-                    });
-                }
-                FLS(`[gotoBlock]: Юхуу...едем к ${targetBlock}`);
-            } else FLS(`[gotoBlock]: Ой ой..Такого блока нет на странице: ${targetBlock}`);
-        };
-        function formFieldsInit(options = {
-            viewPass: false
-        }) {
-            const formFields = document.querySelectorAll("input[placeholder],textarea[placeholder]");
-            if (formFields.length) formFields.forEach((formField => {
-                if (!formField.hasAttribute("data-placeholder-nohide")) formField.dataset.placeholder = formField.placeholder;
-            }));
-            document.body.addEventListener("focusin", (function(e) {
-                const targetElement = e.target;
-                if ("INPUT" === targetElement.tagName || "TEXTAREA" === targetElement.tagName) {
-                    if (targetElement.dataset.placeholder) targetElement.placeholder = "";
-                    if (!targetElement.hasAttribute("data-no-focus-classes")) {
-                        targetElement.classList.add("_form-focus");
-                        targetElement.parentElement.classList.add("_form-focus");
-                    }
-                    formValidate.removeError(targetElement);
-                }
-            }));
-            document.body.addEventListener("focusout", (function(e) {
-                const targetElement = e.target;
-                if ("INPUT" === targetElement.tagName || "TEXTAREA" === targetElement.tagName) {
-                    if (targetElement.dataset.placeholder) targetElement.placeholder = targetElement.dataset.placeholder;
-                    if (!targetElement.hasAttribute("data-no-focus-classes")) {
-                        targetElement.classList.remove("_form-focus");
-                        targetElement.parentElement.classList.remove("_form-focus");
-                    }
-                    if (targetElement.hasAttribute("data-validate")) formValidate.validateInput(targetElement);
-                }
-            }));
-            if (options.viewPass) document.addEventListener("click", (function(e) {
-                let targetElement = e.target;
-                if (targetElement.closest('[class*="__viewpass"]')) {
-                    let inputType = targetElement.classList.contains("_viewpass-active") ? "password" : "text";
-                    targetElement.parentElement.querySelector("input").setAttribute("type", inputType);
-                    targetElement.classList.toggle("_viewpass-active");
-                }
-            }));
-        }
-        let formValidate = {
-            getErrors(form) {
-                let error = 0;
-                let formRequiredItems = form.querySelectorAll("*[data-required]");
-                if (formRequiredItems.length) formRequiredItems.forEach((formRequiredItem => {
-                    if ((null !== formRequiredItem.offsetParent || "SELECT" === formRequiredItem.tagName) && !formRequiredItem.disabled) error += this.validateInput(formRequiredItem);
-                }));
-                return error;
-            },
-            validateInput(formRequiredItem) {
-                let error = 0;
-                if ("email" === formRequiredItem.dataset.required) {
-                    formRequiredItem.value = formRequiredItem.value.replace(" ", "");
-                    if (this.emailTest(formRequiredItem)) {
-                        this.addError(formRequiredItem);
-                        error++;
-                    } else this.removeError(formRequiredItem);
-                } else if ("checkbox" === formRequiredItem.type && !formRequiredItem.checked) {
-                    this.addError(formRequiredItem);
-                    error++;
-                } else if (!formRequiredItem.value) {
-                    this.addError(formRequiredItem);
-                    error++;
-                } else this.removeError(formRequiredItem);
-                return error;
-            },
-            addError(formRequiredItem) {
-                formRequiredItem.classList.add("_form-error");
-                formRequiredItem.parentElement.classList.add("_form-error");
-                let inputError = formRequiredItem.parentElement.querySelector(".form__error");
-                if (inputError) formRequiredItem.parentElement.removeChild(inputError);
-                if (formRequiredItem.dataset.error) formRequiredItem.parentElement.insertAdjacentHTML("beforeend", `<div class="form__error">${formRequiredItem.dataset.error}</div>`);
-            },
-            removeError(formRequiredItem) {
-                formRequiredItem.classList.remove("_form-error");
-                formRequiredItem.parentElement.classList.remove("_form-error");
-                if (formRequiredItem.parentElement.querySelector(".form__error")) formRequiredItem.parentElement.removeChild(formRequiredItem.parentElement.querySelector(".form__error"));
-            },
-            formClean(form) {
-                form.reset();
-                setTimeout((() => {
-                    let inputs = form.querySelectorAll("input,textarea");
-                    for (let index = 0; index < inputs.length; index++) {
-                        const el = inputs[index];
-                        el.parentElement.classList.remove("_form-focus");
-                        el.classList.remove("_form-focus");
-                        formValidate.removeError(el);
-                    }
-                    let checkboxes = form.querySelectorAll(".checkbox__input");
-                    if (checkboxes.length > 0) for (let index = 0; index < checkboxes.length; index++) {
-                        const checkbox = checkboxes[index];
-                        checkbox.checked = false;
-                    }
-                    if (flsModules.select) {
-                        let selects = form.querySelectorAll(".select");
-                        if (selects.length) for (let index = 0; index < selects.length; index++) {
-                            const select = selects[index].querySelector("select");
-                            flsModules.select.selectBuild(select);
-                        }
-                    }
-                }), 0);
-            },
-            emailTest(formRequiredItem) {
-                return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(formRequiredItem.value);
-            }
-        };
-        function formSubmit(options = {
-            validate: true
-        }) {
-            const forms = document.forms;
-            if (forms.length) for (const form of forms) {
-                form.addEventListener("submit", (function(e) {
-                    const form = e.target;
-                    formSubmitAction(form, e);
-                }));
-                form.addEventListener("reset", (function(e) {
-                    const form = e.target;
-                    formValidate.formClean(form);
-                }));
-            }
-            async function formSubmitAction(form, e) {
-                const error = !form.hasAttribute("data-no-validate") ? formValidate.getErrors(form) : 0;
-                if (0 === error) {
-                    const ajax = form.hasAttribute("data-ajax");
-                    if (ajax) {
-                        e.preventDefault();
-                        const formAction = form.getAttribute("action") ? form.getAttribute("action").trim() : "#";
-                        const formMethod = form.getAttribute("method") ? form.getAttribute("method").trim() : "GET";
-                        const formData = new FormData(form);
-                        form.classList.add("_sending");
-                        const response = await fetch(formAction, {
-                            method: formMethod,
-                            body: formData
-                        });
-                        if (response.ok) {
-                            await response.json();
-                            form.classList.remove("_sending");
-                            formSent(form);
-                        } else {
-                            alert("Ошибка");
-                            form.classList.remove("_sending");
-                        }
-                    } else if (form.hasAttribute("data-dev")) {
-                        e.preventDefault();
-                        formSent(form);
-                    }
-                } else {
-                    e.preventDefault();
-                    const formError = form.querySelector("._form-error");
-                    if (formError && form.hasAttribute("data-goto-error")) gotoblock_gotoBlock(formError, true, 1e3);
-                }
-            }
-            function formSent(form) {
-                document.dispatchEvent(new CustomEvent("formSent", {
-                    detail: {
-                        form
-                    }
-                }));
-                setTimeout((() => {
-                    if (flsModules.popup) {
-                        const popup = form.dataset.popupMessage;
-                        popup ? flsModules.popup.open(popup) : null;
-                    }
-                }), 0);
-                formValidate.formClean(form);
-                formLogging(`Форма отправлена!`);
-            }
-            function formLogging(message) {
-                FLS(`[Формы]: ${message}`);
-            }
-        }
         function ssr_window_esm_isObject(obj) {
             return null !== obj && "object" === typeof obj && "constructor" in obj && obj.constructor === Object;
         }
@@ -4976,7 +4743,7 @@
         }
         window.onload = function initSliders() {
             if (document.querySelector(".block-1__slider")) new core(".block-1__slider", {
-                modules: [ Pagination, Autoplay, Lazy ],
+                modules: [ Pagination, Lazy ],
                 observer: true,
                 observeParents: true,
                 slidesPerView: 1,
@@ -5471,7 +5238,7 @@
                 this.scrollWatcherLogging(`Я перестал следить за ${targetElement.classList}`);
             }
             scrollWatcherLogging(message) {
-                this.config.logging ? FLS(`[Наблюдатель]: ${message}`) : null;
+                this.config.logging ? functions_FLS(`[Наблюдатель]: ${message}`) : null;
             }
             scrollWatcherCallback(entry, observer) {
                 const targetElement = entry.target;
@@ -5616,17 +5383,26 @@
         };
         const da = new DynamicAdapt("max");
         da.init();
+        function formSend() {
+            const form = document.getElementById("form");
+            form.addEventListener("submit", formsSend);
+            async function formsSend(e) {
+                e.preventDefault();
+                formVal(form);
+            }
+            function formVal(form) {
+                document.querySelectorAll("[data-required]").forEach((input => {
+                    console.log(input.value);
+                }));
+            }
+        }
+        formSend();
         window["FLS"] = true;
         isWebp();
         addLoadedClass();
         menuInit();
-        fullVHfix();
         spollers();
         tabs();
-        formFieldsInit({
-            viewPass: false
-        });
-        formSubmit();
         headerScroll();
     })();
 })();
